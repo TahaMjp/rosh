2e3bfea3da8b7836be43be576417f367e56af6cf
TahaMajlesi <186780445+TahaMjp@users.noreply.github.com>
2025-06-09 20:38:07 -0700

    update

--- src/Components/ImageComponent/ImageComponent.js
+++ src/Components/ImageComponent/ImageComponent.js
@@ -1,10 +1,25 @@
 const ImageComponent = ({ data }) => {
   return (
-    <div className="flex relative group mb-4 ">
-      <img src={data.photoUrl} alt={data.photoAlt} className="w-full h-auto" />
-      <div className="absolute w-full bg-black h-full opacity-0 group-hover:opacity-60 transition-opacity"></div>
+    <div className="flex relative group mb-4 lg:flex-row flex-col">
       <a
-        className="absolute w-full h-full text-white hidden flex-col items-center justify-center gap-y-4 group-hover:flex transition-transform"
+        className="w-full flex gap-x-3 py-2 lg:hidden"
+        href={data.userLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src={data.userPic}
+          alt={data.userName}
+          className="rounded-full w-1/12"
+        />
+        <p className="font-semibold text-lg flex items-center">
+          {data.userName}
+        </p>
+      </a>
+      <img src={data.photoUrl} alt={data.photoAlt} className="w-full h-auto" />
+      <div className="absolute w-full bg-black h-full opacity-0 group-hover:opacity-60 transition-opacity lg:flex hidden"></div>
+      <a
+        className="absolute w-full h-full text-white hidden flex-col items-center justify-center gap-y-4 lg:group-hover:flex transition-transform"
         href={data.userLink}
         target="_blank"
         rel="noopener noreferrer"
